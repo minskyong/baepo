e8712b9f0274e162a39af6c7c2cdb62023988469
minskyong <fiterpen9@naver.com>
2026-09-17 15:42:51 +0900

    update

--- app.py
+++ app.py
@@ -102,42 +102,46 @@ with col4:
       )
 
 st.divider()
+
+
 if filtered.empty:
       st.warning('조건 맞는 데이터 없음')
+
 else :
- monthly_sales = filtered.groupby('month',as_index=False)['sales'].sum()
 
-left, right = st.columns([2, 1])
+    monthly_sales = filtered.groupby('month',as_index=False)['sales'].sum()
 
-with left :
+    left,right = st.columns([2, 1])
+
+    with left :
         st.subheader('월별매출')
 
-st.line_chart(
-      monthly_sales,
-      x= 'month',
-      y = 'sales'
+        st.line_chart(
+            monthly_sales,
+            x= 'month',
+             y = 'sales'
     )
 
 
 
 
-with right:
-        st.subheader('조회 데이터')
+    with right:
+            st.subheader('조회 데이터')
 
-        st.dataframe(
-            filtered,
-            hide_index=True,
-            column_config={
-                'quantity': st.column_config.NumberColumn(
-                    '판매량',
-                    format='%,d개'
-                ),
-                'sales': st.column_config.NumberColumn(
-                    '매출',
-                    format='%,d원'
-                )
-            }
-        )
+            st.dataframe(
+                filtered,
+                hide_index=True,
+                column_config={
+                    'quantity': st.column_config.NumberColumn(
+                        '판매량',
+                        format='%,d개'
+                    ),
+                    'sales': st.column_config.NumberColumn(
+                        '매출',
+                        format='%,d원'
+                    )
+                }
+            )
 
 
 
